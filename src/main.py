46e57c79959c2db68225e79a584143f231b6a852
Ian Whalen <ian.whalen@gmail.com>
2025-08-10 23:18:50 -0400

    Lint fixes

--- src/main.py
+++ src/main.py
@@ -1,11 +1,8 @@
 import asyncio
+
 from viam.module.module import Module
-try:
-    from models.dht_20 import Dht20
-except ModuleNotFoundError:
-    # when running as local module with run.sh
-    from .models.dht_20 import Dht20
 
+from models.dht_20 import Dht20  # noqa: F401
 
-if __name__ == '__main__':
+if __name__ == "__main__":
     asyncio.run(Module.run_from_registry())
